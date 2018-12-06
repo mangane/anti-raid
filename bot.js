6deb23215e4bafbe9c8777bfb669e83bba56714f
mangane <40431348+mangane@users.noreply.github.com>
2018-12-06 21:13:16 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,12 +14,9 @@ if (message.content === 'salope') {
     message.delete(1)
         message.reply("**change de vocabulaire merci !**");
     }
-if (message.content === '!testou') {
-    message.channel.guild.createChannel('ticket-' + (new Array(4).join('0') + (parseInt(callback[0].case) + 1)).substr(-4), 0, 'New ticket', bot.config.category).then((channel) => {
-          r.table('tickets').insert({
-            id: msg.author.id,
-            case: (new Array(4).join('0') + (parseInt(callback[0].case) + 1)).substr(-4),
-channel: channel.id
+    if (message.content === '!clear') {
+msg.channel.bulkDelete(50); 
+    }
 });
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
