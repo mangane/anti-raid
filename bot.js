65aa1e3c968db673f3b629f10d05d760f5063e6f
mangane <40431348+mangane@users.noreply.github.com>
2018-12-07 17:25:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "!"
+var prefix = "!";
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
