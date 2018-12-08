7a758f12318cbb9ff481dde11ccfeb8ebaebf63e
mangane <40431348+mangane@users.noreply.github.com>
2018-12-08 08:51:18 -1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -100,8 +100,7 @@ client.on(`message`, message =>{
   const pollembed = new Discord.RichEmbed()
   .setTitle("Help commandes")
   .setColor("#5599ff")
-  .setDescription(`Les commandes du bot : 
-!mute (Interdit une personne de communiquer sur le serveur`)
+  .setDescription(`Les commandes du bot : !mute (Interdit une personne de communiquer sur le serveur`)
   .setFooter(`StrawPoll de ${message.author.username}`, `${message.author.avatarURL}`)
   message.channel.sendMessage(pollembed);
       }); 
