8bc73232904e5e5f4a895b494700fbbafb7a285f
TheoZerbibi <33559765+TheoZerbibi@users.noreply.github.com>
2019-03-12 14:53:56 +0100

    Modif

--- bot.js
+++ bot.js
@@ -10,6 +10,22 @@ client.on("ready", () => {
     console.log("Connexion en cours ...");
 });
 
+client.on("ready", () => {
+console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildCreate", guild => {
+console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
 client.on(`message`, message =>{
     if(message.content.startsWith(prefix + "mute")) {
         if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.channel.send("Vous n'avez pas accès à cette commande");
