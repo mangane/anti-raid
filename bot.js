a444bb1209362e9e378957fe9fe686049a92c630
mangane <40431348+mangane@users.noreply.github.com>
2019-03-13 13:58:43 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -94,15 +94,12 @@ client.on(`message`, message =>{
      	.addField("!ping","Permet de voir la réaction du bot")
      	.addField("Informations :","Modérateur [Bêta] crée par ⏳Gaétan#2852");	
      	message.channel.send({embed})	}
-     	if(message.content.startsWith(prefix + "ping")) {
-     		const embed = new
-     		Discord.RichEmbed()
-     		.setColor(0x954D23)
-     		.setTitle("Pong")
-     		.addField("💬","180ms");
-     		message.channel.send({embed})
-     		
-     	}
+     	 
+    if(message.content.startsWith(prefix + "ping")) {
+              const m = await message.channel.send("Ping?");
+              m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+}
+                  
     if(message.content.startsWith(prefix + "kick")) {
         if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.channel.send("Vous n'avez pas accès à cette commande, seul les administrateur on accès à cette commande!");
     
