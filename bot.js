dcd00a200b4c1fd86c97c2886eaf153ea76dadd7
mangane <40431348+mangane@users.noreply.github.com>
2019-03-13 13:51:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,20 +3,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = "!";
 
-client.on("ready", () => {
-console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-client.user.setActivity(`Surveille ${client.guilds.size} serveurs`);
-});
-client.on("guildCreate", guild => {
-console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-client.user.setActivity(`Surveille ${client.guilds.size} serveurs`);
-});
-client.on("guildDelete", guild => {
-  // this event triggers when the bot is removed from a guild.
-  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Surveile ${client.guilds.size} serveurs`);
-});
-
 client.on(`message`, message =>{
     if(message.content.startsWith(prefix + "mute")) {
         if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.channel.send("Vous n'avez pas accès à cette commande");
