e9c9bd17077c72da8b8b880dffa33676a2ec01a9
mangane <40431348+mangane@users.noreply.github.com>
2019-03-13 13:47:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,13 +3,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = "!";
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-client.on("ready", () => {
-    console.log("Connexion en cours ...");
-});
-
 client.on("ready", () => {
 console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
 client.user.setActivity(`Surveille ${client.guilds.size} serveurs`);
