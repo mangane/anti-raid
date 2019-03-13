8c2710ecf1dc6a738316993ffc98709705769101
mangane <40431348+mangane@users.noreply.github.com>
2019-03-13 14:13:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,19 +13,19 @@ client.on("ready", () => {
 client.on("ready", () => {
 console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
 client.user.setActivity(`Regarder ${client.guilds.size} servers`);
-    client.user.setActivity(`Taper !help`);
+    client.user.setActivity(`Taper &help`);
 });
 
 client.on("guildCreate", guild => {
 console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
   client.user.setActivity(`Regarder ${client.guilds.size} servers`);
-     client.user.setActivity(`Taper !help`);
+     client.user.setActivity(`Taper &help`);
 });
 
 client.on("guildDelete", guild => {
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
   client.user.setActivity(`Regarder ${client.guilds.size} servers`);
-     client.user.setActivity(`Taper !help`);
+     client.user.setActivity(`Taper &help`);
 });
 client.on(`message`, message =>{
     if(message.content.startsWith(prefix + "mute")) {
