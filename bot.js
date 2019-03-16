be66e8c17470db0198b13e55641139ca315979ac
HommeDeSable <48624943+HommeDeSable@users.noreply.github.com>
2019-03-16 17:33:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,6 @@ var modo_id = ['516274923828805667','345951306055417857'];
 client.on('ready', () => {
 client.user.setStatus('idle')
   .then(console.log)
-  .catch(console.error);
 }
 });
 client.on('ready', () => {
