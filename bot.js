613b1b01ea410de1f2c097fc1cad39915bcf063f
Xed-Sama <gtdexterd56880@gmail.com>
2019-03-16 15:24:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -122,6 +122,8 @@ client.on(`message`, message =>{
 			await message.react("🤷")
 		});*/
 	} else if(message.content.startsWith(prefix + `quit`) && modo_id.indexOf(message.author.id) != -1) {
+		message.delete();
+		message.channel.send('Je dois quitter sous l'ordre des modérateur du bot!');
     		message.guild.leave()
     			.then(g => console.log(`Left the guild ${g}`))
      			.catch(console.error); 
