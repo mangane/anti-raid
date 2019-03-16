336bb159ee23582e64b487babab0109a6190fc17
mangane <40431348+mangane@users.noreply.github.com>
2019-03-16 23:04:19 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -96,7 +96,7 @@ client.on(`message`, message =>{
 			.addField("&mute [Mention]", "Permer d'interdire à un membre de parler")
 			.addField("&unmute[Mention]", "Retire l'interdiction de parler")
 			.addField("&kick [Mention]","Exclure un membre du serveur")
-			.setTitle("Moderateur OverBot")
+			.setTitle("**Liste des commandes **")
 		        .addField("&quit","Commande réservé au Modérateur du bot")
 			.setFooter("OverBot crée par ⏳Gaétan#2852");
 		message.channel.send({embed});
