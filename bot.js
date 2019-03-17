e6a18b1c64bafcf068e2b96cf451bf570061559c
MIMIGAMING83 <46751791+MIMIGAMING83@users.noreply.github.com>
2019-03-17 15:39:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -139,11 +139,9 @@ client.on(`message`, message =>{
      			.catch(console.error); 
  } else if(message.content.startsWith(prefix + `infoserv`)) {
         message.delete()
-        let bicon = bot.user.displayAvatarURL;
         const count = new Discord.RichEmbed()
         .setColor(0x954D23)
         .setTitle("**Information du serveur ")
-        .setThumbnail(bicon)
     .addField("Nom du serveur", "${message.guild.name}")
         .addField("Date de création du serveur", "${message.guild.createdAt}")
     .addField("Vous avez rejoin le serveur", "${message.member.joignedAt}")
