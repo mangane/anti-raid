1fa7990fcbf1e8d70c1f0ec231d7ca5d4a2795e4
HommeDeSable <48624943+HommeDeSable@users.noreply.github.com>
2019-03-20 16:03:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,13 +14,13 @@ client.user.setStatus('idle')
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`J'ai rejoin un nouveaux serveur : ${guild.name} (id: ${guild.id}). Le serveur à ${guild.memberCount} membre!`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`Taper &help |${client.guilds.size} serveurs`);
 });
 
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`Je ne suis plus sur le serveur : ${guild.name} (id: ${guild.id})`);
-client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`Taper &help |${client.guilds.size} serveurs`);
 });
 client.on('guildMemberAdd', member => {
   const channel = member.guild.channels.find(ch => ch.name === 'Bienvenue');
