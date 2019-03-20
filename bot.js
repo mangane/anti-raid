1060d370bd2a581638482d05f4a5fb510487a6bf
HommeDeSable <48624943+HommeDeSable@users.noreply.github.com>
2019-03-20 23:56:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -195,7 +195,7 @@ if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send(":
     
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!rUser) return message.channel.send("Je ne trouve pas l'utilisateur");
-    let rreason = args.join(" ").slice(27);
+    let rreason = args.join(" ").slice(28);
 
     let reportEmbed = new Discord.RichEmbed()
     .setDescription("Signalement")
