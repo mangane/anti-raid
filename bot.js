8b0929c9994556ab369ea9ecec9709b80189d0a3
HommeDeSable <48624943+HommeDeSable@users.noreply.github.com>
2019-03-21 17:12:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -302,8 +302,6 @@ message.channel.send({embed})
             message.channel.send(`${membre.user.username}à bien été démuter par : ${message.author.username} !`);
     }
 	if(message.content.startsWith(prefix + "gif")) {
-	const embed = new Discord.richEmbed() 
-.setColor(0x00AE86)
         .setImages(`https://media.discordapp.net/attachments/532537522077564928/558308048427548692/Webp.net-gifmaker.gif`);
 }
 });
