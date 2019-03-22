73451dc518cfbbde6134f4be2f7340b32e04f156
frederick1718 <39063322+frederick1718@users.noreply.github.com>
2019-03-22 22:27:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.on('guildMemberAdd', member => {
   if (!channel) return;
     const embed = new Discord.RichEmbed()
     .setColor("#3ad729")
-    .addField("Bienvenue sur le serveur :", `${member}`
+    .addField("Bienvenue sur le serveur :", `${member}`);
 channel.send({embed})
 });
 client.on(`message`, message =>{
