02632b18bee389bdd6df0de671490f543b1d6dbb
SokanLeS <48765683+SokanLeS@users.noreply.github.com>
2019-03-22 14:06:49 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,6 +30,8 @@ client.on('guildMemberAdd', member => {
     const embed = new Discord.RichEmbed()
     .setColor("#3ad729")
     .addField("Bienvenue sur le serveur :", "${member}")
+    .addField("Le serveur possède :","${message.guild.memberCount} de membre")
+channel.send({embed})
 });
 client.on(`message`, message =>{
 	const args = message.content.slice(prefix.length).trim().split(/ +/g);
