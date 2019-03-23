135277a0671e97339321246c4f25d3ef4c7642c1
HommeDeSable <48624943+HommeDeSable@users.noreply.github.com>
2019-03-23 11:42:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -305,5 +305,15 @@ if(message.content.startsWith(prefix + "site")) {
     client.user.setStatus('invisible')
     client.user.setStatus(`idle`);
 }
+	if (args[0].toLowerCase() === prefix + "clear"){
+        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande")
+        let count = args[1]
+        if (!count) return message.channel.send("Veuillez indiquer un nombre de messages à supprimer")
+        if (isNaN(count)) return message.channel.send("Veuillez indiquer un nombre valide")
+        if (count < 1 || count > 100) return message.channel.send("Veuillez indiquer un nombre entre 1 et 100")
+        message.channel.bulkDelete(parseInt(count) + 1)
+        message.channel.send ("J'ai supprimé " + count + " message(s)")
+    }
+})
 });
 client.login(process.env.BOT_TOKEN)
