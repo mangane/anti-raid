1a0565d07d7b87d0378fc1dc70c1c0054ae320c1
HommeDeSable <48624943+HommeDeSable@users.noreply.github.com>
2019-03-23 15:12:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -326,7 +326,8 @@ if(message.content.startsWith(prefix + "stats")) {
     const embed = new Discord.RichEmbed()
     .setColor("#2e1fe6")
     .setTitle("Statistiques")
-    .addField("Nombre de serveur :", client.guilds.size);
+    .addField("Nombre de serveur :", client.guilds.size)
+    .addField("Nombre d'utilisateurs :", client.users.size);
     message.channel.send({embed})
 }
 });
