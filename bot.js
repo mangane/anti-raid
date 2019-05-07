0ade07c7ad61eeaac3719d1126d21c5684eea4f1
mangane <40431348+mangane@users.noreply.github.com>
2019-05-07 17:19:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,7 +66,7 @@ let xoargs = message.content.split(" ").slice(1);
                         .addField("Message :", "**-----------------------------------------------------------**")
                         .addField(`${xo03}`, `**------------------------------------------------------------**`)
                         .setTimestamp()
-                    message.channels.findAll('name', 'inter-trak').map(channel => channel.send(tchat_embed))
+                    client.channels.findAll('name', 'inter-trak').map(channel => channel.send(tchat_embed))
 }
 });
 client.login(process.env.BOT_TOKEN)
