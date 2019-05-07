28c3550db6fbb303b3f088378b96d2d4059a7f5d
mangane <40431348+mangane@users.noreply.github.com>
2019-05-07 17:21:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,24 +49,6 @@ var boobsfr = new Discord.RichEmbed()
 .setImage(`${boobs}`)
 message.channel.send(boobsfr)
 };
-	if (message.content.startsWith (prefix + "globalchat")) {
-let xoargs = message.content.split(" ").slice(1);
-    let xo03 = xoargs.join(" ")
-    var xo02 = message.guild.channels.find('name', 'inter-trak');
-    if (!xo02) return message.reply(`Le salon inter-trak n'existe pas créer le salon ferais marcher la commande`)
-    if (message.channel.name !== 'inter-trak') return message.reply('La commande ne peut être fait que sur le salon inter-trak')
-                if (!xo03) return message.reply("Ecrivez un message")
-                    message.delete();
-                    var tchat_embed = new Discord.RichEmbed()
-                        .setColor("#00ffff")
-                        .setTitle("Chat Global")
-                        .setThumbnail(`${message.author.displayAvatarURL}`)
-                        .addField("Nom d'utilisateur :", message.author.username + "#" + message.author.discriminator, true)
-                        .addField("Du serveur", message.guild.name, true)
-                        .addField("Message :", "**-----------------------------------------------------------**")
-                        .addField(`${xo03}`, `**------------------------------------------------------------**`)
-                        .setTimestamp()
-                    client.channels.findAll('name', 'inter-trak').map(channel => channel.send(tchat_embed))
-}
+	
 });
 client.login(process.env.BOT_TOKEN)
