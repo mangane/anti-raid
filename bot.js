a0d42d9e1846e253993125ebf3c5aea4d6384299
mangane <40431348+mangane@users.noreply.github.com>
2019-05-11 16:30:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -218,11 +218,11 @@ function clean(text) {
       const clean = text => {
 	      if (typeof(text) === "string")
     return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-} else {
+else 
       return text;
           if (typeof(text) === "string")
             return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-         } else {
+         else 
               return text;
         }
 }
