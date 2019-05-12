f28fb94a3e4227659e4c5337b647fd19bc063db3
mangane <40431348+mangane@users.noreply.github.com>
2019-05-13 01:39:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -266,7 +266,6 @@ message.channel.send({embed})
 if(message.content.startsWith(prefix + "delete")) {
 if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("<a:non:576666508571312138>Tu ne peux pas executer la commande demandé");
 message.channel.delete()
-)
 }
 if (message.content.startsWith (prefix + "commu")) {
 	if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("<a:non:576666508571312138>Tu ne peux pas executer la commande demandé");
