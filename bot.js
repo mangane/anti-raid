8918eef2fde377b9e888e7ab21876b0d7802bd74
mangane <40431348+mangane@users.noreply.github.com>
2019-05-15 14:09:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -672,6 +672,14 @@ const embed = new Discord.RichEmbed()
 .addField("Vous voulez voir ce que les commandes font ??? alors rejoignez les serveurs pré faite :"," <:en_ligne:576662449734811659> Qu’il est disponible \n\n<:indisponible:576662605704200192> Qu’il est déjà pris\n\n<:indisponible:576662605704200192> Qu’il est en maintenance\n\n<:offline:576662534585712640>: Qu’il est Fermer\n\nServeur #1 https://discord.gg/Av73Vwg <:en_ligne:576662449734811659>\n\nServeur #2 https://discord.gg/bpZpepc <:en_ligne:576662449734811659> \n\nServeur #3 https://discord.gg/SwuCmmB <:en_ligne:576662449734811659> \n\nServeur #4 https://discord.gg/ <:offline:576662534585712640> \n\nServeur #5 https://discord.gg/ <:offline:576662534585712640>");
 message.channel.send({embed})
 }
+	if(message.content.startsWith(prefix + "contact")) {
+		message.delete()
+var amsg = message.content;
+var msg = amsg.substr(amsg.indexOf(" ") + 1);
+var test = message.guild.channels.find(`id`, "578189974667788289");
+var reason = msg;
+test.send('reason');
+	}
 //<:en_ligne:576662449734811659>
 //<:indisponible:576662605704200192>
 //<:offline:576662534585712640>offline
