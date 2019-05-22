df047906cd4ca4c67f4e567135bbf9c355d0d556
mangane <40431348+mangane@users.noreply.github.com>
2019-05-22 17:32:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -167,6 +167,7 @@ message.channel.send ("<a:la:576804659528990751> Il ne vous reste plus qu'à met
 setTimeout(() => { 
    cooldown.delete(message.author.id);
 }, 720000);
+
 });
 client.on("guildCreate", guild => {
     // This event triggers when the bot joins a guild.
