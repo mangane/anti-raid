8a565fc8d676108b68b1996ec1026c7134e0e5cc
mangane <40431348+mangane@users.noreply.github.com>
2019-05-22 14:26:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -229,10 +229,8 @@ if (message.content.startsWith (prefix + "salon")) {
                 READ_MESSAGES: true,
 		MENTION_EVERYONE: false
    });
-
-	}).catch(console.error); // Send errors to console
-message.channel.send ("<a:la:576804659528990751> Il ne vous reste plus qu'à mettre les salons au bonne endroit, les roles sont à créé avec la commande g!role");
-} {
+})
+} else {
     message.channel.send("⚠️ Le cooldown est activé pendant 1min ! ");
     }
 cooldown.add(message.author.id);
