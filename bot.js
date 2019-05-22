b4422eaea65f5a1276435e208097ea695b23d28b
mangane <40431348+mangane@users.noreply.github.com>
2019-05-22 17:26:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,8 +35,7 @@ message.channel.sendEmbed(embed220)
 });
 client.on("message", async message => {
 if (cooldown.has(message.author.id)) { 
-  message.channel.send("Merci de patientez 10 Minutes");
-} {
+}
 	if (message.content.startsWith (prefix + "salon")) {
 	if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("<a:non:576666508571312138>Tu ne peux pas executer la commande demandé");	
 	message.guild.createRole({
