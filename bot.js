905b58d6e0d6921b2a6d1ce0f275a096d95f4837
mangane <40431348+mangane@users.noreply.github.com>
2019-05-24 17:31:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -164,7 +164,6 @@ if (message.content.startsWith (prefix + "salon")) {
 message.channel.send ("<a:la:576804659528990751> Il ne vous reste plus qu'à mettre les salons au bonne endroit, les roles sont à créé avec la commande g!role");
 	
 } else {
-else {
 if (message.content === prefix + "salon"){
     message.channel.send("⚠️ Le cooldown est activé pendant 1min ! ");
     }
