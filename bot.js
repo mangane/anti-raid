86e1e824c027600c425deb14edeac40301fd56fc
mangane <40431348+mangane@users.noreply.github.com>
2019-05-24 17:52:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,6 +34,7 @@ message.channel.sendEmbed(embed220)
 }
 });
 client.on("message", async message => {
+	if (cooldown.has(message.author.id)) {
 if (message.content.startsWith (prefix + "salon")) {
 	if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("<a:non:576666508571312138>Tu ne peux pas executer la commande demandé");	
 	message.guild.createRole({
@@ -161,6 +162,8 @@ if (message.content.startsWith (prefix + "salon")) {
    });
 	}).catch(console.error); // Send errors to console
 message.channel.send ("<a:la:576804659528990751> Il ne vous reste plus qu'à mettre les salons au bonne endroit, les roles sont à créé avec la commande g!role");
+} else { 
+message.channel.send("non");
 }
 	if (cooldown.has(message.author.id)) { 
 if(message.content.startsWith(prefix + "salon")) {
