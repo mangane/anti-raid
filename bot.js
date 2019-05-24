883d9178031ba81a16d466774270792a3c8a2f91
mangane <40431348+mangane@users.noreply.github.com>
2019-05-24 17:49:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,8 +34,6 @@ message.channel.sendEmbed(embed220)
 }
 });
 client.on("message", async message => {
-	if (cooldown.has(message.author.id)) { 
-	} else {
 if (message.content.startsWith (prefix + "salon")) {
 	if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("<a:non:576666508571312138>Tu ne peux pas executer la commande demandé");	
 	message.guild.createRole({
@@ -164,6 +162,7 @@ if (message.content.startsWith (prefix + "salon")) {
 	}).catch(console.error); // Send errors to console
 message.channel.send ("<a:la:576804659528990751> Il ne vous reste plus qu'à mettre les salons au bonne endroit, les roles sont à créé avec la commande g!role");
 }
+	if (cooldown.has(message.author.id)) { 
 if(message.content.startsWith(prefix + "salon")) {
     message.channel.send("⚠️ Le cooldown est activé pendant 1min ! ");
     }
