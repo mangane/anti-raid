8cc03d680fd3b96cf26c89c8a8d565f06dcd7ce1
mangane <40431348+mangane@users.noreply.github.com>
2019-05-24 17:25:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,6 +34,7 @@ message.channel.sendEmbed(embed220)
 }
 });
 client.on("message", async message => {
+	if (cooldown.has(message.author.id)) {
 	if (message.content.startsWith (prefix + "salon")) {
 	if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("<a:non:576666508571312138>Tu ne peux pas executer la commande demandé");	
 	message.guild.createRole({
@@ -165,7 +166,7 @@ message.channel.send ("<a:la:576804659528990751> Il ne vous reste plus qu'à met
 setTimeout(() => { 
    cooldown.delete(message.author.id);
 }, 720000);
-	}
+	}}
 });
 client.on("guildCreate", guild => {
     // This event triggers when the bot joins a guild.
